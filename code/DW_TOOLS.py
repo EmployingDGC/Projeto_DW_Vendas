aef37b19e541fa33c2dc8fbf0c3e29c71a000573
EmployingDGC <daviguizani@gmail.com>
2021-06-25 16:21:28 -0300

    atualização sugerida DW_TOOLS

--- code/DW_TOOLS.py
+++ code/DW_TOOLS.py
@@ -2,41 +2,42 @@ import pandas as pd
 
 
 def merge_input(left, right, left_on, right_on, surrogate_key, suff):
-    dict_na = right.query(f"{surrogate_key} == -3").to_dict('index')
+    dict_na = right.query(
+        f"{surrogate_key} == -3"
+    ).to_dict('index')
 
-    df = (
-        left.
-            merge(right, how="left", left_on=left_on, right_on=right_on, suffixes=suff).
-            fillna(dict_na[list(dict_na)[0]])
-    )
+    return left.merge(
+        right=right,
+        how="left",
+        left_on=left_on,
+        right_on=right_on,
+        suffixes=suff
+    ).fillna(dict_na[list(dict_na)[0]])
 
-    return df
 
 def create_stage(conn_input, conn_output, schema_in, table, stg_name, tbl_exists):
-
-    (
-        read_table(conn=conn_input, schema=schema_in, table_name=table).
-            to_sql(name=stg_name,
-                   con=conn_output,
-                   schema="STAGE",
-                   if_exists=tbl_exists,
-                   index=False)
+    read_table(
+        conn=conn_input,
+        schema=schema_in,
+        table_name=table
+    ).to_sql(
+        name=stg_name,
+        con=conn_output,
+        schema="STAGE",
+        if_exists=tbl_exists,
+        index=False
     )
 
 
-def dict_to_str(dict):
-    str = list()
-    for value in dict.keys():
-        temp_str = f'{value}" AS "{dict.get(value)}'
-        str.append(temp_str)
+def dict_to_str(dict_):
+    return [f'{k}" AS "{v}' for k, v in dict_.items()]
 
-    return str
 
-def concat_cols(str):
-    if isinstance(str, dict):
-        str = dict_to_str(str)
+def concat_cols(str_):
+    if isinstance(str_, dict):
+        str_ = dict_to_str(str_)
 
-    return '", "'.join(str)
+    return '", "'.join(str_)
 
 
 def read_table(conn, schema, table_name, columns=None, where=None, distinct=False):
@@ -51,9 +52,11 @@ def read_table(conn, schema, table_name, columns=None, where=None, distinct=Fals
         where_clause = f"WHERE {where}"
 
     if columns is None:
-        query = f'SELECT {distinct_clause} * FROM "{schema}"."{table_name}" {where_clause}'
+        columns = "*"
     else:
-        query = f'SELECT {distinct_clause} "{concat_cols(columns)}" FROM "{schema}"."{table_name}" {where_clause}'
+        columns = f'"{concat_cols(columns)}"'
+
+    query = f'SELECT {distinct_clause} {columns} FROM "{schema}"."{table_name}" {where_clause}'
 
     response = pd.read_sql_query(query, conn)
 
