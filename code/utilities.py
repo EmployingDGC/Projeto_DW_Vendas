750a74a5881bd2ed7b65864a3d387e842912dc49
EmployingDGC <daviguizani@gmail.com>
2021-07-13 10:13:19 -0300

    Update utilities.py

--- code/utilities.py
+++ code/utilities.py
@@ -1,12 +1,11 @@
-from sqlalchemy.engine.mock import MockConnection
 import datetime as dt
 
 import pandas as pd
 
 
-def convert_two_list_in_dict(keys: list,
-                             values: list,
-                             is_df: bool = False) -> dict:
+def convert_two_list_in_dict(keys,
+                             values,
+                             is_df=False):
     res = {}
     for key in keys:
         for value in values:
@@ -23,16 +22,16 @@ def convert_two_list_in_dict(keys: list,
     return res
 
 
-def create_index_dataframe(data_frame: pd.DataFrame,
-                           first_index: int = 0) -> list[int]:
+def create_index_dataframe(data_frame,
+                           first_index=0):
     return [i + first_index for i in range(data_frame.shape[0])]
 
 
-def convert_table_to_dataframe(conn_input: MockConnection,
-                               schema_name: str,
-                               table_name: str,
-                               columns: list[str] = None,
-                               qty_parts: int = 100) -> pd.DataFrame:
+def convert_table_to_dataframe(conn_input,
+                               schema_name,
+                               table_name,
+                               columns=None,
+                               qty_parts=100):
     str_columns = ""
 
     if not columns or len(columns) == 0:
@@ -93,8 +92,8 @@ def convert_table_to_dataframe(conn_input: MockConnection,
     return data_frame.reset_index(drop=True)
 
 
-def convert_column_to_float64(column_data_frame: pd.Series,
-                              default: float) -> pd.Series:
+def convert_column_to_float64(column_data_frame,
+                              default):
     def if_1(num):
         return str(num).isnumeric()
 
@@ -117,8 +116,8 @@ def convert_column_to_float64(column_data_frame: pd.Series,
     )
 
 
-def convert_column_to_int64(column_data_frame: pd.Series,
-                            default: int) -> pd.Series:
+def convert_column_to_int64(column_data_frame,
+                            default):
     def if_1(num):
         return str(num).isnumeric()
 
@@ -141,23 +140,23 @@ def convert_column_to_int64(column_data_frame: pd.Series,
     )
 
 
-def convert_column_to_str(column_data_frame: pd.Series) -> pd.Series:
+def convert_column_to_str(column_data_frame):
     return column_data_frame.apply(
         lambda value:
         str(value).strip()
     )
 
 
-def convert_column_datetime_to_date(column_data_frame: pd.Series) -> pd.Series:
+def convert_column_datetime_to_date(column_data_frame):
     return column_data_frame.apply(
         lambda datetime:
         str(datetime).split(" ")[0]
     )
 
 
-def convert_column_to_date(column_data_frame: pd.Series,
-                           format_: str,
-                           default: str) -> pd.Series:
+def convert_column_to_date(column_data_frame,
+                           format_,
+                           default):
     def if_1(date):
         return len(str(date).replace('/', '').replace('-', '').strip()) == 8
 
@@ -172,22 +171,22 @@ def convert_column_to_date(column_data_frame: pd.Series,
     )
 
 
-def convert_column_to_tittle(column_data_frame: pd.Series) -> pd.Series:
+def convert_column_to_tittle(column_data_frame):
     return column_data_frame.apply(
         lambda value:
         str(value).strip().title()
     )
 
 
-def convert_column_to_upper(column_data_frame: pd.Series) -> pd.Series:
+def convert_column_to_upper(column_data_frame):
     return column_data_frame.apply(
         lambda value:
         str(value).strip().upper()
     )
 
 
-def convert_column_cpf_to_int64(column_data_frame: pd.Series,
-                                default: int) -> pd.Series:
+def convert_column_cpf_to_int64(column_data_frame,
+                                default):
     def if_1(cpf):
         str_ = str(cpf).replace("-", "").replace(".", "").replace(" ", "").strip()
 
@@ -205,7 +204,7 @@ def convert_column_cpf_to_int64(column_data_frame: pd.Series,
     )
 
 
-def convert_int_cpf_to_format_cpf(column_data_frame: pd.Series) -> pd.Series:
+def convert_int_cpf_to_format_cpf(column_data_frame):
     return column_data_frame.apply(
         lambda cpf:
         f"{int(str(f'{int(cpf):011}')[:3]):03}"
@@ -215,8 +214,8 @@ def convert_int_cpf_to_format_cpf(column_data_frame: pd.Series) -> pd.Series:
     )
 
 
-def convert_column_cnpj_to_int64(column_data_frame: pd.Series,
-                                 default: int) -> pd.Series:
+def convert_column_cnpj_to_int64(column_data_frame,
+                                 default):
     def if_1(cnpj):
         str_ = str(cnpj).replace("-", "").replace(".", "").replace("/", "").replace(" ", "").strip()
 
@@ -234,7 +233,7 @@ def convert_column_cnpj_to_int64(column_data_frame: pd.Series,
     )
 
 
-def convert_int_cnpj_to_format_cnpj(column_data_frame: pd.Series) -> pd.Series:
+def convert_int_cnpj_to_format_cnpj(column_data_frame):
     return column_data_frame.apply(
         lambda cnpj:
         f"{int(str(f'{int(cnpj):011}')[:2]):02}"
@@ -245,8 +244,8 @@ def convert_int_cnpj_to_format_cnpj(column_data_frame: pd.Series) -> pd.Series:
     )
 
 
-def convert_column_datetime_to_hour(column_data_frame: pd.Series,
-                                    default: int) -> pd.Series:
+def convert_column_datetime_to_hour(column_data_frame,
+                                    default):
     def if_1(date):
         return str(date).split(" ")[1].split(":")[0].isnumeric()
 
@@ -258,8 +257,8 @@ def convert_column_datetime_to_hour(column_data_frame: pd.Series,
     )
 
 
-def convert_column_datetime_to_day(column_data_frame: pd.Series,
-                                   default: int) -> pd.Series:
+def convert_column_datetime_to_day(column_data_frame,
+                                   default):
     def if_1(date):
         return str(date).split(" ")[0].split("-")[2].isnumeric()
 
@@ -271,8 +270,8 @@ def convert_column_datetime_to_day(column_data_frame: pd.Series,
     )
 
 
-def convert_column_datetime_to_month(column_data_frame: pd.Series,
-                                     default: int) -> pd.Series:
+def convert_column_datetime_to_month(column_data_frame,
+                                     default):
     def if_1(date):
         return str(date).split(" ")[0].split("-")[1].isnumeric()
 
@@ -284,8 +283,8 @@ def convert_column_datetime_to_month(column_data_frame: pd.Series,
     )
 
 
-def convert_column_datetime_to_year(column_data_frame: pd.Series,
-                                    default: int) -> pd.Series:
+def convert_column_datetime_to_year(column_data_frame,
+                                    default):
     def if_1(date):
         return str(date).split(" ")[0].split("-")[0].isnumeric()
 
@@ -297,9 +296,9 @@ def convert_column_datetime_to_year(column_data_frame: pd.Series,
     )
 
 
-def insert_row(df: pd.DataFrame,
-               row: int,
-               values: list) -> pd.DataFrame:
+def insert_row(df,
+               row,
+               values):
     return df.iloc[:row, ].append(
         other=pd.DataFrame(
             data=convert_two_list_in_dict(
@@ -315,8 +314,8 @@ def insert_row(df: pd.DataFrame,
     ).reset_index(drop=True)
 
 
-def insert_default_values_table(df: pd.DataFrame,
-                                reset_index: bool = False) -> pd.DataFrame:
+def insert_default_values_table(df,
+                                reset_index=False):
     df_default = pd.DataFrame(
         data={
             k: [-3, -2, -1]
@@ -343,13 +342,13 @@ def insert_default_values_table(df: pd.DataFrame,
     return df_final
 
 
-def multiply_columns(frame: pd.DataFrame,
-                     col_1: str,
-                     col_2: str) -> pd.Series:
+def multiply_columns(frame,
+                     col_1,
+                     col_2):
     return frame[col_1] * frame[col_2]
 
 
-def create_sk_categoria(column: pd.Series) -> pd.Series:
+def create_sk_categoria(column):
     categorias = {
         1: (
             "CAFÉ", "ACHOCOLATADO", "CEREAIS", "PÃO", "AÇÚCAR",
@@ -380,7 +379,7 @@ def create_sk_categoria(column: pd.Series) -> pd.Series:
         )
     }
 
-    def categorized(name: str) -> int:
+    def categorized(name):
         for k, v in categorias.items():
             if name.upper() in v:
                 return k
@@ -398,8 +397,8 @@ def create_sk_categoria(column: pd.Series) -> pd.Series:
     )
 
 
-def create_sk_turno(column: pd.Series) -> pd.Series:
-    def categorized(hour: int) -> int:
+def create_sk_turno(column):
+    def categorized(hour):
         turnos = {
             1: range(6, 12),
             2: range(12, 18),
@@ -419,10 +418,10 @@ def create_sk_turno(column: pd.Series) -> pd.Series:
     )
 
 
-def create_table(conn_output: MockConnection,
-                 schema_name: str,
-                 table_name: str,
-                 table_vars: dict[str, str]) -> None:
+def create_table(conn_output,
+                 schema_name,
+                 table_name,
+                 table_vars: dict[str, str]):
     str_vars = ""
 
     for k, v in table_vars.items():
@@ -433,46 +432,46 @@ def create_table(conn_output: MockConnection,
     conn_output.execute(f"create table if not exists \"{schema_name.lower()}\".\"{table_name}\" ({str_vars})")
 
 
-def create_schema(database: MockConnection,
-                  schema_name: str) -> None:
+def create_schema(database,
+                  schema_name):
     database.execute(f" create schema if not exists {schema_name.lower()}")
 
 
-def drop_tables(conn_output: MockConnection,
-                schema_name: str,
-                dimensions_names: list[str]) -> None:
+def drop_tables(conn_output,
+                schema_name,
+                dimensions_names):
     for i in range(len(dimensions_names)):
         conn_output.execute(
             f" drop table if exists \"{schema_name.lower()}\".\"{dimensions_names[i]}\""
         )
 
 
-def drop_schemas(conn_output: MockConnection,
-                 schemas_names: list[str]) -> None:
+def drop_schemas(conn_output,
+                 schemas_names):
     for schema in schemas_names:
         conn_output.execute(f" drop schema if exists {schema.lower()}")
 
 
-def delete_register_from_table(conn_output: MockConnection,
-                               schema_name: str,
-                               table_name: str,
-                               where: str) -> None:
+def delete_register_from_table(conn_output,
+                               schema_name,
+                               table_name,
+                               where):
     conn_output.execute(
         f"delete from \"{schema_name}\".\"{table_name}\" where {where}"
     )
 
 
-def update_register_from_table(conn_output: MockConnection,
-                               schema_name: str,
-                               table_name: str,
-                               set_: str,
-                               where: str) -> None:
+def update_register_from_table(conn_output,
+                               schema_name,
+                               table_name,
+                               set_,
+                               where):
     conn_output.execute(
         f"update \"{schema_name}\".\"{table_name}\" set {set_} where {where}"
     )
 
 
-def set_ativo(row: pd.Series) -> pd.Series:
+def set_ativo(row):
     row.ativo_x = 0
     row.ativo_y = 0
 
@@ -485,13 +484,13 @@ def set_ativo(row: pd.Series) -> pd.Series:
     return row
 
 
-def compare_two_columns(column_1: pd.Series,
-                        column_2: pd.Series) -> pd.Series:
+def compare_two_columns(column_1,
+                        column_2):
     return column_1 == column_2
 
 
-def generate_date_table(initial_date: str,
-                        final_date: str) -> pd.DataFrame:
+def generate_date_table(initial_date,
+                        final_date):
     format_date = "%Y-%m-%d"
 
     order_columns = [
