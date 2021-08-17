853f93c972ce91d67856f460cb926de727e2db6c
Davi GC <63275906+EmployingDGC@users.noreply.github.com>
2021-08-17 12:57:55 -0300

    alterações f_venda_produto

--- code/F_VENDA_PRODUTO.py
+++ code/F_VENDA_PRODUTO.py
@@ -5,6 +5,8 @@ import DW_TOOLS as dwt
 from pandasql import sqldf
 import datetime as dt
 
+import pandas as pd
+
 from sqlalchemy.types import (
     Integer,
     String,
@@ -20,24 +22,33 @@ def extract_fact_venda_produto(connection):
     :return: dataframe com o merge das stages venda e item_venda
     """
 
-    if utl.table_exists(
+    f_venda_produto_exists = utl.table_exists(
         connection=connection,
         schema_name="dw",
         table_name="f_venda_produto"
-    ):
+    )
+
+    if f_venda_produto_exists:
         query = """
-            select *
-            from stage.stg_venda as sv
-            where date(sv.data_venda) > (
-                select max(fvp.dt_load)
-                from dw.f_venda_produto as fvp
-            )
+            select
+                sv.*
+            from
+                stage.stg_venda as sv
+            where
+                date(sv.data_venda) > (
+                    select
+                        max(fvp.dt_load)
+                    from
+                        dw.f_venda_produto as fvp
+                )
         """
 
     else:
         query = """
-            select *
-            from stage.stg_venda
+            select
+                *
+            from
+                stage.stg_venda
         """
 
     stg_venda = sqldf(
@@ -94,7 +105,7 @@ def extract_fact_venda_produto(connection):
         table_name="stg_item_venda"
     )
 
-    f_venda_produto = stg_venda.assign(
+    merges_temp = stg_venda.assign(
         dt_hora_venda=lambda df: df.data_venda.astype("datetime64[ns]").dt.floor("h")
     ).merge(
         right=stg_item_venda,
@@ -106,30 +117,69 @@ def extract_fact_venda_produto(connection):
         right=d_cliente,
         left_on="id_cliente",
         right_on="cd_cliente",
-        suff=["_03", "_04"],
+        suff=("_03", "_04"),
         surrogate_key="sk_cliente"
     ).pipe(
         func=dwt.merge_input,
         right=d_funcionario,
         left_on="id_func",
         right_on="cd_funcionario",
-        suff=["_03", "_04"],
+        suff=("_05", "_06"),
         surrogate_key="sk_funcionario"
     ).pipe(
         func=dwt.merge_input,
         right=d_tipo_pagamento,
         left_on="id_pagamento",
         right_on="cd_tipo_pagamento",
-        suff=["_03", "_04"],
+        suff=("_07", "_08"),
         surrogate_key="sk_tipo_pagamento"
     ).pipe(
         func=dwt.merge_input,
         right=d_data,
         left_on="dt_hora_venda",
         right_on="dt_referencia",
-        suff=["_03", "_04"],
+        suff=("_09", "_10"),
         surrogate_key="sk_data"
     )
+
+    query = """
+        SELECT
+            mt.*,
+            p.sk_produto,
+            p.vl_percentual_lucro,
+            p.vl_custo,
+            l.sk_loja
+        FROM
+            merges_temp mt
+        LEFT JOIN
+            dw.d_produto p
+        ON
+            mt.id_produto = p.cd_produto
+        AND
+            date(mt.data_venda) >= date(p.dt_vigencia_inicio)
+        AND (
+            date(mt.data_venda) < date(p.dt_vigencia_fim) 
+            OR p.dt_vigencia_fim IS NULL
+        )
+        LEFT JOIN
+            dw.d_loja l
+        ON
+            mt.id_loja = l.cd_loja
+        AND
+            date(mt.data_venda) >= date(l.dt_vigencia_inicio) 
+        AND (
+            date(mt.data_venda) < date(l.dt_vigencia_fim)
+            OR l.dt_vigencia_fim IS NULL
+        )
+    """
+
+    df_venda = sqldf(
+        query=query,
+        env={"merges_temp": merges_temp},
+        db_uri=connection.url
+    )
+
+    return df_venda
 
 
 def treat_fact_venda_produto(frame, connection):
@@ -140,6 +190,41 @@ def treat_fact_venda_produto(frame, connection):
     :return: dataframe com os dados tratados para fazer a fato venda
     """
 
+    select_columns = [
+        "cd_venda",
+        "sk_data",
+        "sk_loja",
+        "sk_cliente",
+        "sk_funcionario",
+        "sk_tipo_pagamento",
+        "sk_produto",
+        "vl_custo_un",
+        "vl_percentual_lucro_un",
+        "vl_bruto_un",
+        "qtd_produto",
+        "nu_nfc",
+        "dt_load"
+    ]
+
+    rename_columns = {
+        "id_venda": "cd_venda"
+    }
+
+    f_venda_produto = frame.assign(
+        sk_produto=lambda df: df.sk_produto.fillna(value=-3).astype("Int64"),
+        sk_loja=lambda df: df.sk_loja.fillna(value=-3).astype("Int64"),
+        vl_custo_un=lambda df: df.vl_custo.str.replace(",", ".").astype("float64"),
+        vl_percentual_lucro_un=lambda df: df.vl_percentual_lucro.str.replace(",", ".").astype("float64"),
+        vl_bruto_un=lambda df: df.vl_custo_un + df.vl_custo_un * df.vl_percentual_lucro_un,
+        nu_nfc=lambda df: df.nfc.astype("int64"),
+        dt_load=pd.to_datetime(dt.datetime.now())
+    ).rename(
+        columns=rename_columns
+    ).filter(
+        items=select_columns
+    )
+
+    return f_venda_produto
 
 
 def load_fact_venda_produto(frame, connection):
@@ -150,19 +235,19 @@ def load_fact_venda_produto(frame, connection):
     """
 
     dtypes = {
-        "sk_produto": Integer(),
-        "sk_cliente": Integer(),
-        "sk_loja": Integer(),
-        "sk_funcionario": Integer(),
+        "cd_venda": Integer(),
         "sk_data": Integer(),
+        "sk_loja": Integer(),
+        "sk_cliente": Integer(),
+        "sk_funcionario": Integer(),
         "sk_tipo_pagamento": Integer(),
-        "sk_endereco_loja": Integer(),
-        "sk_endereco_cliente": Integer(),
-        "cd_nfc": Integer(),
-        "vl_liquido": Float(),
-        "vl_bruto": Float(),
-        "vl_percentual_lucro": Float(),
-        "qtd_produto": Integer()
+        "sk_produto": Integer(),
+        "vl_custo_un": Float(),
+        "vl_percentual_lucro_un": Float(),
+        "vl_bruto_un": Float(),
+        "qtd_produto": Integer(),
+        "nu_nfc": Integer(),
+        "dt_load": DateTime()
     }
 
     frame.to_sql(
